38b35ec6fa186a2a41765139d74546f863cb1c34
Chaemin-Lim <antraxmin@naver.com>
2022-12-29 14:31:46 +0900

    Update urm.ino

--- urm.ino
+++ urm.ino
@@ -24,6 +24,9 @@ void onRequestDistance(byte theAddress, int theDistance)
     Serial.print("Distance:");
     Serial.print(theDistance);
     Serial.println("mm\n\r");
+
+    Serial.print("urm.receivedContent값= ");
+    Serial.println(urm.receivedContent);
 }
 
 void URM_06()
@@ -37,13 +40,15 @@ void URM_06()
             URM_06();    
         }
     }        
+    //Serial.print("urm.receivedContent값= ");
+    //Serial.println(urm.receivedContent);
 }
 
   
 void wheel()
 {
     Serial.println("휠함수 호출");
-    digitalWrite(22,1);
+    Serial.write(1);
     digitalWrite(ENA, HIGH);
     digitalWrite(INA1, HIGH);
     digitalWrite(INA2, LOW);
@@ -65,7 +70,7 @@ void wheel()
     digitalWrite(ENA, LOW);
     digitalWrite(INA1, LOW);
     digitalWrite(INA2, LOW);
-    digitalWrite(22,0);
+    Serial.write(0);
 } 
 
 void compare(int a){ 
@@ -74,24 +79,25 @@ void compare(int a){
     t2=0;
 
     Serial.println(t2-t1);
-
-
+    
     t1 = millis();
     bool check = true;
     while ((urm.receivedContent >= a-5)&&(urm.receivedContent<= a+5) && (check == true)) { 
+      Serial.println("a값= ");
+      Serial.println(a);
       for(i = 0; i < 120; i++) {  // 오류 - 모터 두번 돌아가고 더이상 돌지 않음
-        Serial.println("i값= ");
-        Serial.println(i);
+        //Serial.println("i값= ");
+        //Serial.println(i);
         URM_06(); // 여기까지만 실행됨 (계속 URM_06 실행 => wheel() 실행 반복) 
         myservo.write(i);
         delay(50);
         t2 = millis();
 
-        Serial.println("t2-t1= ");
-         Serial.println(t2-t1);
+//        //Serial.println("t2-t1= ");
+         //Serial.println(t2-t1);
 
         if ((t2-t1) >= 7000){   // 7초가 지나면 동작
-          Serial.println("compare함수 if문 실행");
+          //Serial.println("compare함수 if문 실행");
           wheel();
           check=false;
           break;
@@ -99,6 +105,7 @@ void compare(int a){
         
         else if ((urm.receivedContent <= a-5) && (urm.receivedContent >= a+5))
           break;
+          check=false;
        }   
     }  
 }
@@ -126,24 +133,34 @@ void loop()
   int distant = 0;
 
   //URM_06();
+  URM_06();
+  distant = urm.receivedContent;    // 이 값도 0이었네.. 
+  Serial.print("00:");
+  Serial.print(distant);
+  Serial.println("mm\n\r");
   //URM_06();
-  //URM_06();
-  distant=urm.receivedContent;
-    Serial.print("00:");
-    Serial.print(distant);
-    Serial.println("mm\n\r");
-  //URM_06();
-  //URM_06();
-  //delay(10);
-  if((urm.receivedContent >= distant-5)&&(urm.receivedContent<= distant+5)){
-    compare(distant);
-    }
-  else if(urm.receivedContent > 1000){
-    for(i=0;i<120;i++){ 
-      URM_06();
-      myservo.write(i);
-      delay(50);
-      }}
-  else 
+  
+  // 1번 조건 정상 동작 - 테스트 완료
+  if((urm.receivedContent >= distant-5) && (urm.receivedContent<= distant+5)){
+    Serial.println("1번 조건문 실행");
+    compare(distant); // 오류 - distant가 0값으로 전달되고 있음
+  }
+
+  // 2번 조건
+  else if(urm.receivedContent > 1000) {
+      Serial.println("2번 조건문 실행");
+      for(i = 0; i < 120; i++){ 
+        // Serial.print("i값 = ");
+        // Serial.println(i);
+        URM_06();
+        myservo.write(i);
+        delay(50);
+      }
+  }
+
+  // 3번 조건 -  장애물 측정 거리가 1000 (1m) 이하일때 서보모터 멈추고 1000이하인 곳만 계속해서 초음파 거리 측정
+  else {
+    Serial.println("조건3실행");
     URM_06();
+  }
 }
